65ab0b9d73441cc8536043dce30ccea345560dad
codewizz <c.chaiwat.ch@gmail.com>
2022-10-23 09:09:29 -0700

    initial project

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+    class Database {
+        private $host = 'localhost';
+        private $db_name = 'myblog';
+        private $username = 'root';
+        private $password = 'tutor123';
+        private $conn;
+
+        public function connect()
+        {
+            $this->conn = null;
+            try {
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            } catch (PDOException $e) {
+                var_dump("Connect Error: {$e->getMessage()}");
+                exit();
+            }
+            
+            return $this->conn;
+        }
+    }
---
+++ take_ama.sql
@@ -0,0 +1,48 @@
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `userType` int(5) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `user_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `user_type` (`id`, `type`) VALUES
+(1, 'elderly'),
+(2, 'caretaker'),
+(3, 'admin')
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `birthDay` int(5) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `lastUpdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isActive` int(1) NOT NULL,
+  `startWorkDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `endWorkDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+CREATE TABLE `order_transection` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `careTakerId` int(11) NOT NULL,
+  `hours` int(5) NOT NULL,
+  `price` DECIMAL(10, 2) NOT NULL,
+  `status` int(5) NOT NULL,
+  `amaId` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
