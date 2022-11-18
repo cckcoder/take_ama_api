892dced2b64e779f3d0a3109d3fc9b79683034e3
codewizz <c.chaiwat.ch@gmail.com>
2022-11-18 23:15:19 +0700

    add: news delete

--- models/NewsModel.php
+++ models/NewsModel.php
@@ -94,4 +94,26 @@
                 return false;
             }
         }
+
+        public function delete()
+        {
+            $query = "DELETE FROM {$this->table} WHERE id = :id";
+            // Prepare statement
+            $stmt = $this->conn->prepare($query);
+            // Clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+            // Bind data
+            $stmt->bindParam(':id', $this->id);
+            // Execute query
+            if ($stmt->execute())
+            {
+                return true;
+            }
+            else
+            {
+                print_r("Error: {$stmt->error}");
+                return false;
+            }
+
+        }
     }
